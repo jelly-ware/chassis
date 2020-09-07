eabd64ab5a8cae2468335c89dc5b0e3eba1b2c04
Judah Donkor <judahdonkor@gmail.com>
2020-09-07 18:08:12 +0000

    Clean up stash

--- src/main/java/org/jellyware/chassis/Stash.java
+++ src/main/java/org/jellyware/chassis/Stash.java
@@ -19,21 +19,12 @@ public class Stash {
         this.svc = svc;
     }
 
-    public URI of(String name, boolean att) {
+    public URI of(String id, boolean att) {
         return svc.of(Optional.of(List.of(PATH, name)), (att ? Optional.of(Stash.ATT) : Optional.empty()),
                 Optional.empty());
     }
 
-    public URI of(Long id, boolean att) {
-        return svc.of(Optional.of(List.of(PATH, id.toString())), (att ? Optional.of(Stash.ATT) : Optional.empty()),
-                Optional.empty());
-    }
-
-    public URI of(String name) {
+    public URI of(String id) {
         return of(name, false);
-    }
-
-    public URI of(Long id) {
-        return of(id, false);
     }
 }
